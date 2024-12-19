4247a45e5b4d8f9a62bc675e2c7865aa73066796
Kumar Anirudha <mail@anirudha.dev>
2024-12-19 16:51:47 +0530

    entrypoint resolved

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![cfg_attr(target_arch = "wasm32", no_main, no_std)]
 
-pub use libtokensale::stylus_entrypoint;
+pub use libtokensale::user_entrypoint as stylus_entrypoint;
 
 #[cfg(not(target_arch = "wasm32"))]
 #[doc(hidden)]
